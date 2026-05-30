4a2630c7608c34bc279f3f2899bc4bfe958ed483
Black-tempest <camara.baba.b71808057@gmail.com>
2026-05-30 01:17:32 +0000

    Update developer attribution in menu command
    Signed-off-by: Black-tempest <camara.baba.b71808057@gmail.com>

--- CommandFiles/commands/menu.ts
+++ CommandFiles/commands/menu.ts
@@ -555,7 +555,7 @@ export async function entry({
       `${UNISpectra.arrowFromT} View by page: **${prefix}${commandName} <page>**`,
       `${UNISpectra.arrowFromT} View the basics: **${prefix}${commandName} basics**`,
 
-      `${UNISpectra.disc} Developed by @**Liane Cagara** 🎀`,
+      `${UNISpectra.disc} Developed by @**Veldora Tempest** 🌹`,
     ].join("\n");
     if (1) {
       return output.replyStyled(strs, {
